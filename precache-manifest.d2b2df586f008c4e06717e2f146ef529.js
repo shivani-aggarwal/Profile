6f0dfdd9e2a12e51b7533f224c13eafe3ba6ed90
Shivani Aggarwal <shivanisa33@gmail.com>
2019-01-11 23:03:38 -0500

    Updates

---
+++ precache-manifest.d2b2df586f008c4e06717e2f146ef529.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "85906be27e1c86cecdc1960ecefcc011",
+    "url": "/profile/static/media/downArrow.85906be2.png"
+  },
+  {
+    "revision": "0580efdcc4eb20def045",
+    "url": "/profile/static/css/main.94735464.chunk.css"
+  },
+  {
+    "revision": "e07dd8ee390670286a606e57e1fc6edb",
+    "url": "/profile/static/media/todoey.e07dd8ee.png"
+  },
+  {
+    "revision": "6b0d6d2337fcf611fb15",
+    "url": "/profile/static/js/1.6b0d6d23.chunk.js"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "2a3e51376612a21b240d32f76be4f099",
+    "url": "/profile/static/media/logo.2a3e5137.png"
+  },
+  {
+    "revision": "0580efdcc4eb20def045",
+    "url": "/profile/static/js/main.0580efdc.chunk.js"
+  },
+  {
+    "revision": "fb531a5fb0b893e366a409ac0872ed2a",
+    "url": "/profile/static/media/rightArrow.fb531a5f.png"
+  },
+  {
+    "revision": "6ae9f77fe64a5c67747cd494b0ede239",
+    "url": "/profile/static/media/sotaro.6ae9f77f.png"
+  },
+  {
+    "revision": "e65f6f50360b0ea4c12820f55814945f",
+    "url": "/profile/static/media/readykit.e65f6f50.png"
+  },
+  {
+    "revision": "6b0d6d2337fcf611fb15",
+    "url": "/profile/static/css/1.fe51c6bb.chunk.css"
+  },
+  {
+    "revision": "0227cd211eb24ab3518ea75fc464d802",
+    "url": "/profile/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea10b4770f5d77d3f4971daebdae35a4.js"
+  "/profile/precache-manifest.d2b2df586f008c4e06717e2f146ef529.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/profile/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
